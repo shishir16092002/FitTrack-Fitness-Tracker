101ea9b0a46587973d1cd2189e5a4e6312a0b3bf
Shishir Yadav <yadavshishir16@gmail.com>
2025-06-28 00:00:08 +0530

    add updates

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const path = require("path");
 const apiRoutes = require("./api");
 
-router.use("/api", apiRoutes);
+router.use("/", apiRoutes);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,29 +1,25 @@
 const express = require("express");
+const path = require("path");
 const routes = require("./routes");
 const db = require("./config/connection");
 const cors = require('cors');
-require("dotenv").config();
+require("dotenv").config(); 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// CORS Setup
 app.use(cors({
-  origin: process.env.FRONTEND_ORIGIN || '*',  
+  origin: process.env.FRONTEND_ORIGIN || '*',
   credentials: true,
 }));
 
-// API Routes
 app.use("/api", routes);
 
-// DB and Server Start
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`✅ MongoDB connected successfully!`);
     console.log(`API server running on port ${PORT}!`);
   });
 });
