4bcd27bf58bf11f11b7e4d3f01676cf99cf00102
Shishir Yadav <yadavshishir16@gmail.com>
2025-06-27 22:51:21 +0530

    update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,4 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
-// connect with react router
-// serve up react front-end in production
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../../build/index.html"));
-});
-
 module.exports = router;
